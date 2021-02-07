9d69b5bbbb7a1da5de15ed9c67b5879efb6fea84
zainokta <zainokta@gmail.com>
2021-02-07 18:35:00 +0700

    fix(redis): Fix redis url to become dynamic

--- fetcher/src/infrastructure/redis.js
+++ fetcher/src/infrastructure/redis.js
@@ -2,6 +2,6 @@ const redis = require('redis')
 const bluebird = require('bluebird')
 
 bluebird.promisifyAll(redis)
-const redisClient = redis.createClient()
+const redisClient = redis.createClient(process.env.REDIS_HOST)
 
 module.exports = redisClient
