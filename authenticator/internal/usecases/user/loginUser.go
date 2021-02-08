287ea6c0d8a736c09bae82630133bac25ba97e27
zainokta <zainokta@gmail.com>
2021-02-08 09:01:52 +0700

    refactor(login): change error response on fail to find phone or incorrect password

--- authenticator/internal/usecases/user/loginUser.go
+++ authenticator/internal/usecases/user/loginUser.go
@@ -4,6 +4,7 @@ import (
 	"authenticator/internal/core"
 	"authenticator/internal/entity"
 	"authenticator/internal/utils"
+	"errors"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -11,11 +12,11 @@ import (
 func LoginUser(request *core.LoginRequest, userRepository entity.IUserRepository) (string, error) {
 	user, err := userRepository.FindByPhone(request.Phone)
 	if err != nil {
-		return "", err
+		return "", errors.New("Invalid Credentials")
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password)); err != nil {
-		return "", err
+		return "", errors.New("Invalid Credentials")
 	}
 
 	token, err := utils.GenerateJWTFromUser(user)
